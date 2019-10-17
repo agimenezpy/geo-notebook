fab53a04422432cd11e901f03d35ce23c413bb63
Alberto Gimenez <albergimenez@gmail.com>
2019-10-17 00:54:27 -0300

    Ejemplos dados en clase

---
+++ Ejemplos/mi_modulo.py
@@ -0,0 +1,7 @@
+# Importamos el modulo sys
+import sys
+
+# Version del sistem operativo
+print(sys.version)
+
+sys.exit(1)
---
+++ Ejemplos/mi_modulo_2.py
@@ -0,0 +1,8 @@
+
+albums = {
+    "Yes" : ["Close to the", "Fragile"],
+    "Genesis": ["Foxtrot", "The Nursery"],
+    "ELP": ["Brain"]
+}
+
+version = 1
